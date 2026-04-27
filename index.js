f8b6edccfa139364ad2df7a66284355ee8f81ac6
Ubuntu <ubuntu@ip-172-31-11-119.eu-north-1.compute.internal>
2026-04-27 20:40:19 +0000

    test webhook

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Mohan 🚀 from your own app');
+  res.send('auto build working');
 });
 
 app.listen(3000, () => {
